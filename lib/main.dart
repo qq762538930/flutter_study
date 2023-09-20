00ca0e2a0fb36935eb3a69d3364333fbfbb99701
qq762538930 <762538930@qq.com>
2023-09-20 14:32:37 +0800

    add

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+void main() {
+  Human a = Human('张三', 88, 9);
+  Human b = Human('李四', 66, 90); // tag1
+  Student c = Student('王麻子', 113, 99, school: '恒大华府');
+
+  print(c.info());
+
+  // print(a.info());
+  // print(b.name);
+}
+
+//定义一个人类
+class Human {
+  String name = '';
+  double weight = 0;
+  double height = 0;
+
+  Human(this.name, this.weight, this.height);
+
+  String info() {
+    return "我是 $name";
+  }
+}
+
+// 定义一个学生继承人类
+// super 指向父级
+class Student extends Human {
+  final String school;
+
+  Student(super.name, super.weight, super.height, {required this.school});
+  @override
+  String info() {
+    String info ="${super.info()}school:$school";
+    // 此处返回的是重写过后的Student info
+    return info;
+  }
+}
